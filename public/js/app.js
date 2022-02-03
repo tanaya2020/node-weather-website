334823983478673dfcf6d73628063b7af11518ac
Tanaya Patra <tanayapatra2020@gmail.com>
2022-02-03 23:00:52 +0530

    Set up app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,24 @@
-const weatherForm = document.querySelector("form");
-const search = document.querySelector("input");
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
 
-weatherForm.addEventListener("submit", (e) => {
-  e.preventDefault();
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
 
-  const location = search.value;
+    const location = search.value
 
-  fetch("/weather?address=" + location).then((response) => {
-    response.json().then((data) => {
-      if (data.error) {
-        console.log(data.error);
-      } else {
-        console.log(data.location);
-        console.log(data.forecastData);
-      }
-    });
-  });
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
 
-  console.log(location);
-});
+    fetch('/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
+    })
+})
